caff0c84e924c0acf46f4774e26e4a5f431398e5
2win9s <57966737+2win9s@users.noreply.github.com>
2020-01-06 22:25:51 +0000

    Update INH.py

--- INH.py
+++ INH.py
@@ -1,4 +1,12 @@
 # MachineLearning.Research.Project.Of.Errors?Albert?
 #Insert Name Here = I.N.H.
+import numpy as np
 import useful_things_tools_when_using_this_mess
-eulerno
+eulerno(100000000000000)
+inputgen( )
+listttt = [ ]
+numofnugen(listttt)
+hiddenlayergen()
+outputgen( )
+weightgen()
+biasgen()
