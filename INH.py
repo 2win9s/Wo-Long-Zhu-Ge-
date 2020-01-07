7ad200d1b818be3535ae85bf0c041d4daef80833
2win9s <57966737+2win9s@users.noreply.github.com>
2020-01-07 14:11:08 +0000

    Update INH.py

--- INH.py
+++ INH.py
@@ -3,10 +3,10 @@
 import numpy as np
 import useful_things_tools_when_using_this_mess
 eulerno(100000000000)
-inputgen( )
-listttt = [ ]
+inputgen(1600)
+listttt = [700,160,70,42,32,18,14]
 numofnugen(listttt)
 hiddenlayergen()
-outputgen( )
+outputgen(13)
 weightgen()
 biasgen()
