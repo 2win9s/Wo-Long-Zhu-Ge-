4b146cf3bd65a394f1a60693634b74032770c5af
2win9s <57966737+2win9s@users.noreply.github.com>
2020-01-12 22:50:07 +0000

    Update qlearn.py

--- qlearn.py
+++ qlearn.py
@@ -1,5 +1,16 @@
 import numpy as np
 
+def states(x):
+  global states
+  states = np.append(states,x,axis = 1)
+  
+def actions(x):
+  global actions
+  actions = np.zeros(shape = x)
+  
+def qtable():
+  global actions
+  global states
+  qtable = np.zeros(shape = [shape.size,len(action)])
 
-def qtableinit(s,a):            #s is the number of states and a is the number of actions
-  qtable = np.zeros(shape = [s,a])
+
