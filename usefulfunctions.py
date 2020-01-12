261c0ee73c3f4e61eb74044a81e71edb8cdc4d01
2win9s <57966737+2win9s@users.noreply.github.com>
2020-01-12 22:40:49 +0000

    Update usefulfunctions.py

--- usefulfunctions.py
+++ usefulfunctions.py
@@ -1,4 +1,4 @@
-
+# initialize 4 prime numbers preferablly very big ones rng, x , m , r ,z
 rng = 5915587277
 def random(x,m,r,z):                           #x,rand m should be rediculously big prime numbers,z is the larget number you want out of it
   global rngout
