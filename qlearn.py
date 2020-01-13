43c9eac532df5da605bc26aef544fc7e106f05ca
2win9s <57966737+2win9s@users.noreply.github.com>
2020-01-13 21:30:42 +0000

    Update qlearn.py

--- qlearn.py
+++ qlearn.py
@@ -1,16 +1,35 @@
 import numpy as np
+import notrandom.py as nr
 
-def states(x):
+def statesinit():
   global states
-  states = np.append(states,x,axis = 1)
+  states = numpy.array(shape = 0,0)
   
-def actions(x):
+  
+def actions(x,y):                             #y is a list of actions e.g. y = ["up","down","left","right","1","2","3","4","5","6","7","8","9"]
   global actions
   actions = np.zeros(shape = x)
+  for r in range(y):
+    actions[r] = y[r]
+
+def qtableinit():
+  global qtable
   
-def qtable():
-  global actions
+def qtableaddstate(x):
   global states
-  qtable = np.zeros(shape = [shape.size,len(action)])
+  global actions
+  global qtable
+  r = 0
+  for k in range(len(states)):
+    if x == states[k]:
+      r = r + 1
+    else:
+      r = r + 0
+  if r == 0:
+    states = np.append(states,x,axis = 1)
+  rh = np.zeros(shape = len(actions))
+  for x in range(len(rh)):
+    rh[x] = nr.decing(7369130657357778596659,400000010149,195327895579,1,6)
+  qtable = np.append(qtable,rh,axis = 1)
 
 
