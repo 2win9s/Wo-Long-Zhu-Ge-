f41bc276e69e0c16d75b2e28d63d12f98d6f0f99
2win9s <57966737+2win9s@users.noreply.github.com>
2020-01-27 19:58:41 +0000

    Update model2.py

--- scifest/model2.py
+++ scifest/model2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import basicML as bc
 import pickle
-#input list of 18 subjects output 13 different points you cn get as a result of your grade,[0,12,20,28,37,46,56,71,81,91,102,]
+#input list of 18 subjects output 13 different points you cn get as a result of your grade,[0,12,20,28,37,46,56,71,81,91,102,113,125]
 numofhl = [16,14]
 bc.inputgen(18)
 bc.hiddenlayersgen()
