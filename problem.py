39f72faf061db58e8b8c76ca3bb738387c941417
2win9s <57966737+2win9s@users.noreply.github.com>
2020-01-27 23:14:35 +0000

    Create problem.py

---
+++ problem.py
@@ -0,0 +1,2 @@
+import numpy as np
+so in the folder T.A.N.N. in the file basicML.py i have backpropgation as a recursive process, will an iterative process be faster in this situaton?, is it possibe to make iterative?
