741437b2242b754b2c9d272f3f310789a8cf1587
2win9s <57966737+2win9s@users.noreply.github.com>
2020-01-27 17:40:15 +0000

    Update model1.py

--- scifest/model1.py
+++ scifest/model1.py
@@ -1,3 +1,10 @@
 import numpy as np
 import basicML as bc
 import pickle
+#input list of 18 subjects output 11 different points you cn get as a result pf your grade,[0,12,20,28,37,46,56,66,77,88,100], here we exclude the higher level + 25 points
+numofhl = [16,14]
+bc.inputgen(18)
+bc.hiddenlayersgen()
+bc.outputgen(11)
+bc.weightsgen()
+bc.biasgen()
