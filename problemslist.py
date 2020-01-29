e6657ffc0491372b687bbdd6c5e1da108cbd7cbc
AmethystCash <58050303+AmethystCash@users.noreply.github.com>
2020-01-29 12:41:48 +0000

    Update problemslist.py
    made that change

--- problemslist.py
+++ problemslist.py
@@ -2,4 +2,3 @@ so in the folder T.A.N.N. in the file basicML.py i have backpropgation as a recu
 in this situaton?, is it possibe to make iterative?
 also plenty of rookie mistakes bad practices and too many global variables but most of the code does what it is supposed to do
 and how do you re-pickle a variable to get an updated version of it??
-should we try make a visual out of how many gens have been completed in each run? (ive made it print "completed x runs" when its finished)
