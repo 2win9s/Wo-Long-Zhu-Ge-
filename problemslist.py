0639649ee220a768df66631dfd1ac63027d63693
AmethystCash <58050303+AmethystCash@users.noreply.github.com>
2020-01-29 11:12:57 +0000

    Update problemslist.py

--- problemslist.py
+++ problemslist.py
@@ -1,4 +1,5 @@
-import numpy as np
-so in the folder T.A.N.N. in the file basicML.py i have backpropgation as a recursive process, will an iterative process be faster in this situaton?, is it possibe to make iterative?
+so in the folder T.A.N.N. in the file basicML.py i have backpropgation as a recursive process, will an iterative process be faster 
+in this situaton?, is it possibe to make iterative?
 also plenty of rookie mistakes bad practices and too many global variables but most of the code does what it is supposed to do
 and how do you re-pickle a variable to get an updated version of it??
+should we try make a visual out of how many gens have been completed in each run? (ive made it print "completed x runs" when its finished)
