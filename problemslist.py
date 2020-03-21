7bfea6af2f0f9b302d798de43a2bce2b36ef2c75
2win9s <57966737+2win9s@users.noreply.github.com>
2020-03-21 22:42:58 +0000

    Delete problemslist.py

--- problemslist.py
+++
@@ -1,4 +0,0 @@
-so in the folder T.A.N.N. in the file basicML.py i have backpropgation as a recursive process, will an iterative process be faster 
-in this situaton?, is it possibe to make iterative?
-also plenty of rookie mistakes bad practices and too many global variables but most of the code does what it is supposed to do
-and how do you re-pickle a variable to get an updated version of it??
