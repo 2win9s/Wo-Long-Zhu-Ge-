1209d9a15098c801ab0d0a53c3f217c93d288e3f
2win9s <57966737+2win9s@users.noreply.github.com>
2020-07-04 22:00:51 +0000

    Update init.cpp

--- project ba_zhen_tu/echo test/init.cpp
+++ project ba_zhen_tu/echo test/init.cpp
@@ -19,17 +19,17 @@ int NNs;                                                //number of neurons
 std::vector<float> NN;                                       //the Neural Network is represented as a vector
 
 
-std::vector<vector<int>> W1i;                                //weights pt1 index of input neuron
-std::vector<vector<int>> W2i;                                //weights pt2 index of input
-std::vector<vector<float>> W1s;                              //the multiplier of the weight
-std::vector<vector<float>> W2s;                              //the multiplier of the weight
+std::vector<std::vector<int>> W1i;                                //weights pt1 index of input neuron
+std::vector<std::vector<int>> W2i;                                //weights pt2 index of input
+std::vector<std::vector<float>> W1s;                              //the multiplier of the weight
+std::vector<std::vector<float>> W2s;                              //the multiplier of the weight
 
-std::vector<vector<int>> rW1i;                               //same connections put in different order for easier concurrent backpropagation
-std::vector<vector<int>> rW2i;                                
-std::vector<vector<float>> rW1s;                              
-std::vector<vector<float>> rW2s;                               
+std::vector<std::vector<int>> rW1i;                               //same connections put in different order for easier concurrent backpropagation
+std::vector<std::vector<int>> rW2i;                                
+std::vector<std::vector<float>> rW1s;                              
+std::vector<std::vector<float>> rW2s;                               
 
-std::vector<vector<int>> layermap;                           //map of fastlane into layers
+std::vector<std::vector<int>> layermap;                           //map of fastlane into layers
 
 std::vector<float> bias;                                     //bias
 
@@ -49,7 +49,7 @@ int Lthreadz;
 template<typename nu>
 void notnum(nu num){
     if(num == 0){
-        cout<<"you entered 0 or you didn't enter a number"<<endl;
+        std::cout<<"you entered 0 or you didn't enter a number"<<std::endl;
         exit (EXIT_FAILURE);
     }
 }
@@ -58,11 +58,11 @@ void notnum(nu num){
 void syncinit(){
     unsigned long long int list = NNs;
     int itr = 0;
-    vector<int> layer = {};
+    std::vector<int> layer = {};
     int ind = 0;
     layermap.emplace_back(layer);
-    static vector<int> layertrack;
-    static vector<int> neuronindx;
+    static std::vector<int> layertrack;
+    static std::vector<int> neuronindx;
     layertrack.resize(NN.size());
     neuronindx.resize(NN.size());
     #pragma omp parallel num_threads(Lthreadz) proc_bind(spread)
@@ -83,8 +83,8 @@ void syncinit(){
         unsigned long long int avc1;
         double chance1;
         double chance2;
-        normal_distribution<double> dis(0,rconnect_sdeviation);
-        uniform_real_distribution<double> tri(0.0,1.0);
+        std::normal_distribution<double> dis(0,rconnect_sdeviation);
+        std::uniform_real_distribution<double> tri(0.0,1.0);
         #pragma omp master
         {
             tr = false;
@@ -109,7 +109,7 @@ void syncinit(){
                 chance2 = connectn2 - floor(connectn2);
                 connectn2 = (chance2>tri(twisting)) ? (floor(connectn2) + 1): floor(connectn2);
                 connectn2 = (connectn2<avc2) ? connectn2:avc2;
-                normal_distribution<float> Xavier(0,sqrt(2.0/(connectn2 + 1)));
+                std::normal_distribution<float> Xavier(0,sqrt(2.0/(connectn2 + 1)));
                 W2i[0].emplace_back(NNs-1);
                 W2s[0].emplace_back(abs(Xavier(twisting)));
                 for(unsigned long long int i = 0; i < connectn2;++i){
@@ -168,7 +168,7 @@ void syncinit(){
             connectn2 = (chance2>tri(twisting)) ? (floor(connectn2) + 1): floor(connectn2);
             connectn = (connectn<avc1) ? connectn:avc1;
             connectn2 = (connectn2<avc2) ? connectn2:avc2;
-            normal_distribution<float> Xavier(0,sqrt(2.0/(connectn + connectn2 + 1)));
+            std::normal_distribution<float> Xavier(0,sqrt(2.0/(connectn + connectn2 + 1)));
             W1i[y].emplace_back(y-1);
             W1s[y].emplace_back(abs(Xavier(twisting)));
             for(unsigned long long int i = 0; i < connectn2;++i){
@@ -312,19 +312,19 @@ void syncinit(){
 inline void inputscan(){
     int length;
     int ind;
-    cout<<"enter number of input neurons"<<endl;
-    cin>>length;
+    std::cout<<"enter number of input neurons"<<std::endl;
+    std::cin>>length;
     if(length > NNs){
-        cout<<"error: not enough neurons, entered number is greater than total number of neurons"<<endl;
+        std::cout<<"error: not enough neurons, entered number is greater than total number of neurons"<<std::endl;
         exit (EXIT_FAILURE);
     }
     notnum(length);
-    cout<<"enter input neuron indices"<<endl;
+    std::cout<<"enter input neuron indices"<<std::endl;
     for(int x = 0; x < length; ++x){
-        cout<<"enter input neuron index (remember vector indexing starts from 0)"<<endl;
-        cin>>ind;
+        std::cout<<"enter input neuron index (remember vector indexing starts from 0)"<<std::endl;
+        std::cin>>ind;
         if((ind >= NNs) || (ind < 0)){
-            cout<<"error: index out of range"<<endl;
+            std::cout<<"error: index out of range"<<std::endl;
             exit (EXIT_FAILURE);
         }
         else{
@@ -338,19 +338,19 @@ inline void inputscan(){
 inline void outputscan(){
     int length;
     int ind;
-    cout<<"enter number of output neurons"<<endl;
-    cin>>length;
+    std::cout<<"enter number of output neurons"<<std::endl;
+    std::cin>>length;
     if(length > NNs){
-        cout<<"error: not enough neurons, entered number is greater than total number of neurons"<<endl;
+        std::cout<<"error: not enough neurons, entered number is greater than total number of neurons"<<std::endl;
         exit (EXIT_FAILURE);
     }
     notnum(length);
-    cout<<"enter input neuron indices"<<endl;
+    std::cout<<"enter input neuron indices"<<std::endl;
     for(int x = 0; x < length; ++x){
-        cout<<"enter output neuron index (remember vector indexing starts from 0)"<<endl;
-        cin>>ind;
+        std::cout<<"enter output neuron index (remember vector indexing starts from 0)"<<std::endl;
+        std::cin>>ind;
         if((ind >= NNs) || (ind < 0)){
-            cout<<"error: index out of range"<<endl;
+            std::cout<<"error: index out of range"<<std::endl;
             exit (EXIT_FAILURE);
         }
         else
@@ -363,40 +363,40 @@ inline void outputscan(){
 
 //creates the binary files
 void savebinf(){
-    ofstream layermapbin("layermapbin",ofstream::trunc); 
+    std::ofstream layermapbin("layermapbin",std::ofstream::trunc); 
     boost::archive::binary_oarchive  ilayermapbin(layermapbin); 
     ilayermapbin & layermap; 
-    ofstream W2ibin("W2i.bin",ofstream::trunc); 
+    std::ofstream W2ibin("W2i.bin",std::ofstream::trunc); 
     boost::archive::binary_oarchive  iW2bin(W2ibin); 
     iW2bin & W2i; 
-    ofstream W1ibin("W1i.bin",ofstream::trunc); 
+    std::ofstream W1ibin("W1i.bin",std::ofstream::trunc); 
     boost::archive::binary_oarchive  iW1bin(W1ibin); 
     iW1bin & W1i;
-    ofstream rW2ibin("rW2i.bin",ofstream::trunc); 
+    std::ofstream rW2ibin("rW2i.bin",std::ofstream::trunc); 
     boost::archive::binary_oarchive  riW2bin(rW2ibin); 
     riW2bin & rW2i; 
-    ofstream rW1ibin("rW1i.bin",ofstream::trunc); 
+    std::ofstream rW1ibin("rW1i.bin",std::ofstream::trunc); 
     boost::archive::binary_oarchive  riW1bin(rW1ibin); 
     riW1bin & rW1i;
-    ofstream W1sbin("W1s.bin",ofstream::trunc); 
+    std::ofstream W1sbin("W1s.bin",std::ofstream::trunc); 
     boost::archive::binary_oarchive  sW1bin(W1sbin); 
     sW1bin & W1s; 
-    ofstream W2sbin("W2s.bin",ofstream::trunc); 
+    std::ofstream W2sbin("W2s.bin",std::ofstream::trunc); 
     boost::archive::binary_oarchive  sW2bin(W2sbin); 
     sW2bin & W2s;
-    ofstream rW1sbin("rW1s.bin",ofstream::trunc); 
+    std::ofstream rW1sbin("rW1s.bin",std::ofstream::trunc); 
     boost::archive::binary_oarchive  rsW1bin(rW1sbin); 
     rsW1bin & rW1s; 
-    ofstream rW2sbin("rW2s.bin",ofstream::trunc); 
+    std::ofstream rW2sbin("rW2s.bin",std::ofstream::trunc); 
     boost::archive::binary_oarchive  rsW2bin(rW2sbin); 
     rsW2bin & rW2s;
-    ofstream biasbin("bias.bin",ofstream::trunc); 
+    std::ofstream biasbin("bias.bin",std::ofstream::trunc); 
     boost::archive::binary_oarchive  biasesbin(biasbin); 
     biasesbin & bias;
-    ofstream inputibin("inputi.bin",ofstream::trunc); 
+    std::ofstream inputibin("inputi.bin",std::ofstream::trunc); 
     boost::archive::binary_oarchive  iinputbin(inputibin); 
     iinputbin & inputi;  
-    ofstream outputibin("outputi.bin",ofstream::trunc); 
+    std::ofstream outputibin("outputi.bin",std::ofstream::trunc); 
     boost::archive::binary_oarchive  ioutputbin(outputibin); 
     ioutputbin & outputi;  
 }
@@ -404,40 +404,40 @@ void savebinf(){
 
 //creates the xml files
 void savexmlf(){ 
-    ofstream layermapxml("layermap.xml",ofstream::trunc);
+    std::ofstream layermapxml("layermap.xml",std::ofstream::trunc);
     boost::archive::xml_oarchive ilayermapxml(layermapxml);
     ilayermapxml & BOOST_SERIALIZATION_NVP(layermap); 
-    ofstream W2ixml("W2i.xml",ofstream::trunc);  
+    std::ofstream W2ixml("W2i.xml",std::ofstream::trunc);  
     boost::archive::xml_oarchive  iW2xml(W2ixml);  
     iW2xml & BOOST_SERIALIZATION_NVP(W2i); 
-    ofstream W1ixml("W1i.xml",ofstream::trunc);  
+    std::ofstream W1ixml("W1i.xml",std::ofstream::trunc);  
     boost::archive::xml_oarchive  iW1xml(W1ixml);  
     iW1xml & BOOST_SERIALIZATION_NVP(W1i);   
-    ofstream W1sxml("W1s.xml",ofstream::trunc);  
+    std::ofstream W1sxml("W1s.xml",std::ofstream::trunc);  
     boost::archive::xml_oarchive  sW1xml(W1sxml);  
     sW1xml & BOOST_SERIALIZATION_NVP(W1s);  
-    ofstream W2sxml("W2s.xml",ofstream::trunc);  
+    std::ofstream W2sxml("W2s.xml",std::ofstream::trunc);  
     boost::archive::xml_oarchive  sW2xml(W2sxml);  
     sW2xml & BOOST_SERIALIZATION_NVP(W2s);  
-    ofstream rW2ixml("rW2i.xml",ofstream::trunc);  
+    std::ofstream rW2ixml("rW2i.xml",std::ofstream::trunc);  
     boost::archive::xml_oarchive  riW2xml(rW2ixml);  
     riW2xml & BOOST_SERIALIZATION_NVP(rW2i); 
-    ofstream rW1ixml("rW1i.xml",ofstream::trunc);  
+    std::ofstream rW1ixml("rW1i.xml",std::ofstream::trunc);  
     boost::archive::xml_oarchive  riW1xml(rW1ixml);  
     riW1xml & BOOST_SERIALIZATION_NVP(rW1i);   
-    ofstream rW1sxml("rW1s.xml",ofstream::trunc);  
+    std::ofstream rW1sxml("rW1s.xml",std::ofstream::trunc);  
     boost::archive::xml_oarchive  rsW1xml(rW1sxml);  
     rsW1xml & BOOST_SERIALIZATION_NVP(rW1s);  
-    ofstream rW2sxml("rW2s.xml",ofstream::trunc);  
+    std::ofstream rW2sxml("rW2s.xml",std::ofstream::trunc);  
     boost::archive::xml_oarchive  rsW2xml(rW2sxml);  
     rsW2xml & BOOST_SERIALIZATION_NVP(rW2s);
-    ofstream biasxml("bias.xml",ofstream::trunc);  
+    std::ofstream biasxml("bias.xml",std::ofstream::trunc);  
     boost::archive::xml_oarchive  biasesxml(biasxml);  
     biasesxml & BOOST_SERIALIZATION_NVP(bias);   
-    ofstream inputixml("inputi.xml",ofstream::trunc);  
+    std::ofstream inputixml("inputi.xml",std::ofstream::trunc);  
     boost::archive::xml_oarchive  iinputxml(inputixml);  
     iinputxml & BOOST_SERIALIZATION_NVP(inputi); 
-    ofstream outputixml("outputi.xml",ofstream::trunc);  
+    std::ofstream outputixml("outputi.xml",std::ofstream::trunc);  
     boost::archive::xml_oarchive  ioutputxml(outputixml);  
     ioutputxml & BOOST_SERIALIZATION_NVP(outputi);   
 }
@@ -446,36 +446,35 @@ void savexmlf(){
 int main(){
     clock_t r = clock();
     omp_set_dynamic(0);
-    cout<<"CHECK IF YOU HAVE ENOUGH MEMORY (in heap), always estimate worst case memory usage and have much more than enough memory available."<<endl;
-    cout<<endl;
-    cout<<endl;
-    cout<<"enter number of logical processors/number of threads you are allowed concurrently"<<endl;
-    cin>>Lthreadz;
+    std::cout<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<"enter number of logical processors/number of threads you are allowed concurrently"<<std::endl;
+    std::cin>>Lthreadz;
     notnum(Lthreadz);
     omp_set_num_threads(Lthreadz);
-    cout<<"enter number of neurons"<<endl;
-    cin>>NNs;
+    std::cout<<"enter number of neurons"<<std::endl;
+    std::cin>>NNs;
     notnum(NNs);
     inputscan();
     outputscan();
-    cout<<"enter connect base for initialisation"<<endl;
-    cin>>connect_base;
-    cout<<"enter rconnectrate standard deviation"<<endl;
-    cin>>rconnect_sdeviation;
-    cout<<"enter rconnectrate cap "<<endl;
-    cin>>connect_cap;
+    std::cout<<"enter connect base for initialisation"<<std::endl;
+    std::cin>>connect_base;
+    std::cout<<"enter rconnectrate standard deviation"<<std::endl;
+    std::cin>>rconnect_sdeviation;
+    std::cout<<"enter rconnectrate cap "<<std::endl;
+    std::cin>>connect_cap;
     omp_set_num_threads(Lthreadz);
     clock_t t = clock();
-    cout<<"wait..."<<endl;
-    vector<float> vec(NNs,0);
+    std::cout<<"wait..."<<std::endl;
+    std::vector<float> vec(NNs,0);
     NN = vec;
     bias = vec;
-    vector<vector<int>> vec1(NNs);
+    std::vector<std::vector<int>> vec1(NNs);
     W1i = vec1;
     W2i = vec1;
     rW1i = vec1;
     rW2i = vec1;
-    vector<vector<float>> vec2(NNs);
+    std::vector<std::vector<float>> vec2(NNs);
     W1s = vec2;
     W2s = vec2;
     rW1s = vec2;
@@ -484,44 +483,44 @@ int main(){
     vec1.clear();
     vec2.clear();
     syncinit();
-    string ttt;
+    std::string ttt;
     t = clock() - t;
     double time_elapsed = ((double)t) / CLOCKS_PER_SEC;
-    cout << time_elapsed << " seconds to complete initialisation" << endl;
+    std::cout << time_elapsed << " seconds to complete initialisation" << std::endl;
     while(true){
-        cout<<"type xml for xml file, bin for binary files or both for both xml and binary files"<<endl;
-        cin>>ttt;
+        std::cout<<"type xml for xml file, bin for binary files or both for both xml and binary files"<<std::endl;
+        std::cin>>ttt;
         if(ttt == "xml"){
             clock_t r = clock();
-            cout<<"wait..."<<endl;
+            std::cout<<"wait..."<<std::endl;
             savexmlf();
             t = t + (clock() - r);
             break;
         }
         else if(ttt == "bin"){
             clock_t r = clock();
-            cout<<"wait..."<<endl;
+            std::cout<<"wait..."<<std::endl;
             savebinf();
             t = t + (clock() - r);
             break;
         }
         else if(ttt == "both"){
             clock_t r = clock();
-            cout<<"wait..."<<endl;
+            std::cout<<"wait..."<<std::endl;
             savexmlf();
             savebinf();
             t = t + (clock() - r);
             break;
         }
         else{
-            cout<<"error: invalid input; enter xml for xml file, bin for binary files (omit the .), or both for copies of both file types"<<endl;
+            std::cout<<"error: invalid input; enter xml for xml file, bin for binary files (omit the .), or both for copies of both file types"<<std::endl;
         }
     }
-    cout<<"files have been created"<<endl;
+    std::cout<<"files have been created"<<std::endl;
     double time_taken = ((double)t) / CLOCKS_PER_SEC;
-    cout << time_taken << " seconds to complete all tasks" << endl;
+    std::cout << time_taken << " seconds to complete all tasks" << std::endl;
     r = clock() - r;
     time_taken = ((double)r) / CLOCKS_PER_SEC;
-    cout << time_taken << " seconds total time" << endl;
+    std::cout << time_taken << " seconds total time" << std::endl;
     return 0;
 }
